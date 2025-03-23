eca516edb3bdcb2ad4d83af904342c61c069e9f5
saradhi288 <saradhisai27@gmail.com>
2025-03-23 22:19:20 +0530

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -9,9 +9,7 @@ public class App {
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
-
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        scan.close();
+        System.out.printf("%d\n%d\n%d\n",a,b,c);
     }
 }
